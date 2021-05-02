a793535d40aaf68b38b121cf2ba8777cdbbf619b
luispiura89 <luispiura@unitec.edu>
2021-05-02 16:09:49 -0600

    Move `ManagedCache` and `ManagedFeedImage` to their own files.

---
+++ FeedStoreChallenge/ManagedCache.swift
@@ -0,0 +1,49 @@
+//
+//  ManagedCache.swift
+//  FeedStoreChallenge
+//
+//  Created by Luis Francisco Piura Mejia on 5/2/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedCache)
+internal final class ManagedCache: NSManagedObject {
+	@NSManaged var timestamp: Date
+	@NSManaged var feed: NSOrderedSet
+
+	var localFeed: [LocalFeedImage] {
+		feed.array.compactMap {
+			$0 as? ManagedFeedImage
+		}.map {
+			LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+		}
+	}
+
+	static func uniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+		try find(in: context).map(context.delete)
+
+		return ManagedCache(context: context)
+	}
+
+	static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+		let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+		let fetchResult = try context.fetch(request)
+		return fetchResult.first
+	}
+
+	static func getFeed(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+		return NSOrderedSet(array: feed.map {
+			let image = ManagedFeedImage(context: context)
+
+			image.id = $0.id
+			image.imageDescription = $0.description
+			image.location = $0.location
+			image.url = $0.url
+
+			return image
+		})
+	}
+}
---
+++ FeedStoreChallenge/ManagedFeedImage.swift
@@ -0,0 +1,19 @@
+//
+//  ManagedFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Luis Francisco Piura Mejia on 5/2/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedFeedImage)
+internal final class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
+}
